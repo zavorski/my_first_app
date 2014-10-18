dff924ae315b471c7f8f0df95f8b03c4a6c78857
zavorski <zavorski@gmail.com>
2014-10-18 16:50:19 -0400

    third update

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,9 +7,9 @@ class TweetsController < ApplicationController
 	def create
 		@tweet_to_create = Tweet.new(tweet_params)
 		if @tweet_to_create.save
-			flash["success"] = "You have created a tweet"
+			flash.now["success"] = "You have created a tweet"
 		else
-			flash["danger"] = "Please review the errors below."
+			flash.now["danger"] = "Please review the errors below."
 		end
 
 		@tweet = Tweet.new
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ActiveRecord::Base
 
-validates :content, length: { maximum: 10 }
+validates :content, length: { maximum: 140 }
 
 end
