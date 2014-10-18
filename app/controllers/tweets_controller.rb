76db51878d1d22deca3aecaae658dbbd127d61e4
zavorski <zavorski@gmail.com>
2014-10-18 16:03:57 -0400

    created first tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,8 +5,10 @@ class TweetsController < ApplicationController
 	end
 
 	def create
-		@tweet = Tweet.new(tweet_params)
-		@tweet.save
+		@tweet_to_create = Tweet.new(tweet_params)
+		@tweet_to_create.save
+
+		@tweet = Tweet.new
 
 		@tweets = Tweet.all
 
